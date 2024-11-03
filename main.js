9932617f79ba086976cce3d619c6b05deae9c742
kamui481 <reception@idealinfo.jp>
2024-11-03 10:22:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const testDataDisplay = document.getElementById('testDataDisplay');
 
 // 入力されるコード（途中まで入力されるものを複数設定）
 const codeSamples = [
-  `
+  
 import sys
 from collections import defaultdict
 
@@ -21,8 +21,8 @@ def add_dicts(d1, d2):
             d1[k] += d2[k]
         else:
             d1[k] = d2[k]
-  `,
-  `
+  ,
+  
 from collections import deque
 
 def bfs(maze, start, goal, rows, cols):
@@ -41,18 +41,18 @@ def bfs(maze, start, goal, rows, cols):
             nx, ny = x + dx, y + dy
 
             if 0 <= nx < rows and 0 <= ny < cols and (nx, ny) not in visited and maze[nx][ny] != '1':
-  `
+  
 ];
 
 // テストデータの定義
 const testCases = [
   {
     input: "abcdefg10h12(ij2(3k))l9mnop4(3(2(6(qq)r)s5tu)7v5w)x15(yz)",
-    expected: `a 1\nb 1\nc 1\nd 1\ne 1\nf 1\ng 1\nh 10\ni 12\nj 12\nk 72\nl 1\nm 9\nn 1\no 1\np 1\nq 288\nr 24\ns 12\nt 60\nu 12\nv 28\nw 20\nx 1\ny 15\nz 15`
+    expected: a 1\nb 1\nc 1\nd 1\ne 1\nf 1\ng 1\nh 10\ni 12\nj 12\nk 72\nl 1\nm 9\nn 1\no 1\np 1\nq 288\nr 24\ns 12\nt 60\nu 12\nv 28\nw 20\nx 1\ny 15\nz 15
   },
   {
     input: "10000(10000(10000(2000(ab)500(dz)c200h)2mu3000(fpr)))",
-    expected: `a 2000000000000000\nb 2000000000000000\nc 1000000000000\nd 500000000000000\ne 0\nf 300000000000\ng 0\nh 200000000000000\ni 0\nj 0\nk 0\nl 0\nm 200000000\nn 0\no 0\np 300000000000\nq 0\nr 300000000000\ns 0\nt 0\nu 100000000\nv 0\nw 0\nx 0\ny 0\nz 500000000000000`
+    expected: a 2000000000000000\nb 2000000000000000\nc 1000000000000\nd 500000000000000\ne 0\nf 300000000000\ng 0\nh 200000000000000\ni 0\nj 0\nk 0\nl 0\nm 200000000\nn 0\no 0\np 300000000000\nq 0\nr 300000000000\ns 0\nt 0\nu 100000000\nv 0\nw 0\nx 0\ny 0\nz 500000000000000
   },
   {
     input: "4 5\n0 s 0 1\n0 0 1 0\n0 1 1 0\n0 0 1 g\n0 0 0 0",
@@ -92,7 +92,7 @@ function startTypingAnimation() {
 // テストデータを表示し、裏で完全な処理を実行して結果を出力
 function displayTestData() {
   const currentTest = testCases[testIndex];
-  testDataDisplay.textContent = `入力データ:\n${currentTest.input}`;
+  testDataDisplay.textContent = 入力データ:\n${currentTest.input};
   
   // 完全な処理を模擬して出力
   setTimeout(() => {
